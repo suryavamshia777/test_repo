66e93234dcb5562639812064ebdaa8856e05cd8c
Surya vamshi Amaranayani <suryavamshiamaranayani@Surya-MacBook-Pro.local>
2023-10-25 21:27:47 -0600

    aa

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ class Player(Turtle):
         super().__init__()
         self.shape("turtle")
         self.penup()
-        self.goto(STARTING_POSITION)
+        self.goto(STAaRTING_POSITION)
         self.setheading(90)
     
     def startpoint(self):
